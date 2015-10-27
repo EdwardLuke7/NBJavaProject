9b9fb06747023a033069adc494509ad1654afe89
EdwardLuke7 <ewer.wtsonline@gmail.com>
2015-10-27 17:35:42 +0000

    Complete view, checkout and update

--- del/WOTS/src/OrderLineDAO.java
+++
@@ -1,6 +0,0 @@
-
-import java.util.ArrayList;
-
-public interface OrderLineDAO {
-	public ArrayList<OrderLine> fetchAll(CustomerOrder order);
-}
--- del/WOTS/src/ProductDAO.java
+++
@@ -1,7 +0,0 @@
-
-import java.util.ArrayList;
-
-public interface ProductDAO {
-	public Product fetch(int id);
-	public ArrayList<Product> fetchAll();
-}
